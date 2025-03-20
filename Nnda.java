51887365f454ce835305ad921e13b9d4594a2619
Srikar Goud <nemurisrikargoud@gmail.com>
2025-03-20 21:21:50 +0530

    Update and rename name and age.java to Nnda.java

--- Nnda.java
+++ Nnda.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Main {
-    public static void main(String[] args) {
+    public static void Nnda(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter your name and age:");
     System.out.println(sc.next()+ " is " +sc.next()+ " years old");
